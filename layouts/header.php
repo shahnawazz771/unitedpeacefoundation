933b6652a95ec562cbacb6355389c18c10c39206
Nausher Khan - i712700k <nausher.khan12@gmail.com>
2023-11-09 18:08:18 +0530

    latest changes

--- layouts/header.php
+++ layouts/header.php
@@ -8,7 +8,7 @@
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header col-md-2">
               <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse" aria-expanded="false"> <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
-              <a class="navbar-brand" href="index.php"><img src="images/logo1.gif" width="100%" class="img-responsive"></a>
+              <a class="navbar-brand" href="index.php"><img src="images/logo1.gif" width="100%"></a>
             </div>
             <div class="col-md-3">
               <div class="oraganization">
@@ -18,23 +18,24 @@
             </div>
             <div class="col-md-7">
               <div class="collapse navbar-collapse" id="navbar-collapse">
+                <a href="javascript:void(0)" class="header-bar-icon" res-action="y"><i class="fa fa-bars"></i></a>
                 <ul class="nav navbar-nav navbar-right">
                   <li class="home"><a href="index.php?page=home">Home</a></li>
                   <li class="dropdown aboutus"><a class="dropdown-toggle" data-toggle="dropdown" href="#">About Us <span class="caret"></span></a>
-                    <ul class="dropdown-menu dropdown-menu-left">
+                    <ul class="dropdown-menu dropdown-menu-left dropdown-1st">
                       <li><a href="our-trust.php?page=aboutus">About our trust</a></li>
                       <li><a href="vision.php?page=aboutus">Vision, Mission</a></li>
                     </ul>
                   </li>
                   <li class="dropdown getinvolved"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Get Involved <span class="caret"></span></a>
-                    <ul class="dropdown-menu dropdown-menu-left">
+                    <ul class="dropdown-menu dropdown-menu-left dropdown-2nd">
                       <li><a href="donate.php?page=getinvolved">How to Donate</a></li>
                       <li><a href="sponser-an-orphen.php?page=getinvolved">Sponsor, Orphan, Sponsorship</a></li>
                       <li><a href="volunteer.php?page=getinvolved">Volunteer With Us</a></li>
                     </ul>
                   </li>
                   <li class="dropdown programme"><a class="dropdown-toggle" data-toggle="dropdown" href="#">programme <span class="caret"></span></a>
-                    <ul class="dropdown-menu dropdown-menu-left">
+                    <ul class="dropdown-menu dropdown-menu-left dropdown-3rd">
                       <li><a href="orphan-sponsorship-program.php?page=programme">Orphan Sponsorship programme</a></li>
                       <li><a href="educational-program.php?page=programme">Educational programme</a></li>
                       <li><a href="food-program.php?page=programme">Food programme</a></li>
