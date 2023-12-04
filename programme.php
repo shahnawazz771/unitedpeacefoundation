95115f0beda68c7352d6fcf02f638e099771f692
Nausher Khan - i712700k <nausher.khan12@gmail.com>
2023-12-04 15:15:16 +0530

    latest cahnges

---
+++ programme.php
@@ -0,0 +1,75 @@
+
+<?php include "layouts/html_starting.php"; ?>
+
+<title>Programme</title>
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+
+<?php include "layouts/header.php" ?>
+
+<div class="background-image-3" style="background-image:url(images/about-us.jpg);">
+  <div class="container padding-top-bottom">
+    <div class="row white">
+      <div class="col-md-12">
+        <div class="absolute-text">
+          <h2 class="text-center programme-sub-title-header orphanspopro">programme > Orphan Sponsorship programme</h2>
+          <h2 class="text-center programme-sub-title-header edupro">programme > Educational programme</h2>
+          <h2 class="text-center programme-sub-title-header foodpro">programme > Food programme</h2>
+          <h2 class="text-center programme-sub-title-header emergencyrel">programme > Emergency Relief</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="container padding-top-bottom-100 text-center programme-sub-title-main orphanspopromain">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="text-center"><b>Orphan Sponsorship programme</b></h2>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12">
+      <p><b>Orphan sponsorship is a vital lifeline for many vulnerable young children. There are many unfortunate children who do not have loving support of a family. Children whose shelter is open sky and bed is a piece of cardboard box, living day to day with the risk of abuse and exploitation.</b></p>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12"> <img src="images/gallery-4.jpg" width="50%"> </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12">
+      <p><b>Our Orphan sponsorship programmeme changes the life of these children in some of the most deprived communities in India. With your support we can ensure that they receive education, food, healthcare &medical support and loving care that any child needs.</b></p>
+    </div>
+  </div>
+</div>
+<div class="container padding-top-bottom-100 text-center programme-sub-title-main edupromain">
+  <div class="row">
+    <div class="col-md-12">
+      <h2 class="text-center"><b>Orphan Sponsorship programme</b></h2>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12">
+      <p><b>Orphan sponsorship is a vital lifeline for many vulnerable young children. There are many unfortunate children who do not have loving support of a family. Children whose shelter is open sky and bed is a piece of cardboard box, living day to day with the risk of abuse and exploitation.</b></p>
+    </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12"> <img src="images/gallery-4.jpg" width="50%"> </div>
+  </div>
+  <br>
+  <div class="row">
+    <div class="col-md-12">
+      <p><b>Our Orphan sponsorship programmeme changes the life of these children in some of the most deprived communities in India. With your support we can ensure that they receive education, food, healthcare &medical support and loving care that any child needs.</b></p>
+    </div>
+  </div>
+</div>
+
+<?php include "layouts/footer.php"; ?>
+<?php include "layouts/html_ending.php"; ?>
