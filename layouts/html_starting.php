d83d0c4b9ff11411f4992f1fb8cfff725fa52b5f
Nausher <nausher_khan@gmail.com>
2023-12-12 12:09:15 +0530

    latest changes

--- layouts/html_starting.php
+++ layouts/html_starting.php
@@ -6,4 +6,5 @@
 <link href="css/style.css" rel="stylesheet" type="text/css">
 <link href="css/main.css" rel="stylesheet" type="text/css">
 <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <link href="css/responsive.css" rel="stylesheet" type="text/css">
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<meta name="keywords" content="United Peace Foundation is a non-profit organization established to help and uplift the most vulnerable, orphans, destitute and widows regardless of faith race or creed.">
